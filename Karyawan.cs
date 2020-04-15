c4df20a85ef828b4912b2799deabdbc16fd9901f
daffarafikanza <61929389+daffarafikanza@users.noreply.github.com>
2020-04-15 12:25:57 +0700

    Add files via upload

---
+++ Karyawan.cs
@@ -0,0 +1,37 @@
+﻿namespace ResponsiPemograman2826
+{
+    public class Karyawan
+    {
+        private int NIK { get; set; }
+        private string Nama { get; set; }
+        private int GajiBulanan { get; set; }
+
+        public Karyawan(int NIK, string Nama, int Gaji)
+        {
+            this.NIK = NIK;
+            this.Nama = Nama;
+
+            if (GajiBulanan < 0)
+            {
+                this.GajiBulanan = 0;
+            }
+            else
+            {
+                this.GajiBulanan = Gaji;
+            }
+        }
+
+        public void showKaryawan()
+        {
+            Console.WriteLine("{0}\t {1}\t {2}", NIK, Nama, GajiBulanan);
+        }
+
+        public void naikGaji()
+        {
+            double tmp = 0;
+            tmp = 0.1 * GajiBulanan;
+            GajiBulanan += Convert.TOInt32(tmp);
+            Console.WriteLine("{0}\t {1}\t {2}", NIK, Nama, GajiBulanan);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ResponsiPemrograman2826
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Karyawan karyawan = new Karyawan(190302123, "Paijo", 10000000);
+            Karyawan karyawan2 = new Karyawan(190302124, "Jono", -50000000);
+
+
+            Console.WriteLine("NIK\t Nama\t Gaji");
+            Console.WriteLine("--------------------------");
+            karyawan.showKaryawan();
+            karyawan2.showKaryawan();
+
+            Console.WriteLine("\nAsiiikkk naik gaji nih");
+
+            Console.WriteLine("NIK\t Nama\t Gaji");
+            Console.WriteLine("--------------------------");
+            karyawan.naikGaji();
+            karyawan2.naikGaji();
+
+            Console.ReadKey();
+        }
+    }
+}
